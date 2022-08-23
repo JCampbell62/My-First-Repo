b108d330c4e5fdc82db68e74271a4f530552382f
JCampbell62 <111463304+JCampbell62@users.noreply.github.com>
2022-08-23 12:39:37 -0500

    Started a BlueJ Project

---
+++ BlueJ Project/chapter1Notes.java
@@ -0,0 +1,41 @@
+
+/**
+ * Jack Campbell
+ * 8/23/2022
+ * Notes for Ch 1
+ * Write a description: where we are going to take some code notes
+ * Who helped me: Declan Micheal O'Donovan, son of Neil Joeseph O'Donovan
+ * 
+ * 
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class chapter1Notes
+{
+    /**
+     * main method
+     * where we are going to code
+     */
+    public static void main(String[] args)
+    {
+        
+        Scanner input = new Scanner(System.in);
+        
+        //these are the basic data types
+        int x=9,y,z;
+        double num = 5, num1 = 23.4567, num2;
+        char letter = 'd';
+        boolean choice = false;
+        float anum = 7;
+        long a = 9;
+        short b = 5;
+        
+        //These are objects!
+        String word = new String("My name in there");
+        Integer number = new Integer(47);
+        Double number2 = new Double(34.5);
+        
+    }
+}
